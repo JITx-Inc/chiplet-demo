4fffe3480d077fac270f94969a98aa1f7eb8da8a
Bhusan Gupta <bgupta@thronax.com>
2022-08-10 14:04:39 -0700

    interim checkin

--- update_ocdb.sh
+++ update_ocdb.sh
@@ -2,14 +2,14 @@
 
 # run once
 # git remote add -f ocdb git@github.com:JITx-Inc/open-components-database.git
-# git subtree add -P open-components-database ocdb dev --squash
+# git subtree add -P open-components-database ocdb main --squash
 #
 # run to update ocdb at will
-git subtree pull -P open-components-database ocdb dev --squash
+git subtree pull -P open-components-database ocdb main --squash
 
 # rm -R open-components-database
-# git subtree add -P open-components-database git@github.com:JITx-Inc/open-components-database.git dev --squash
-# git subtree pull -P open-components-database git@github.com:JITx-Inc/open-components-database.git dev --squash
+# git subtree add -P open-components-database git@github.com:JITx-Inc/open-components-database.git main --squash
+# git subtree pull -P open-components-database git@github.com:JITx-Inc/open-components-database.git main --squash
 
 # old code from @philippe
 # cp -R ../../../open-components-database open-components-database
