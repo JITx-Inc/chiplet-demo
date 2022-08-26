e77d0a3dab70cb56111727fca8c4557cd621e2f4
Bhusan Gupta <bgupta@thronax.com>
2022-08-25 23:46:46 -0700

    getting ready for a simple demo

--- update_ocdb.sh
+++ update_ocdb.sh
@@ -2,18 +2,18 @@
 
 # run once
 # git remote add -f ocdb git@github.com:JITx-Inc/open-components-database.git
-# git subtree add -P open-components-database ocdb main --squash
+# git subtree add -P ocdb ocdb main --squash
 #
 # run to update ocdb at will
-git subtree pull -P open-components-database ocdb main --squash
+git subtree pull -P ocdb ocdb main --squash
 
 # rm -R open-components-database
-# git subtree add -P open-components-database git@github.com:JITx-Inc/open-components-database.git main --squash
-# git subtree pull -P open-components-database git@github.com:JITx-Inc/open-components-database.git main --squash
+# git subtree add -P ocdb git@github.com:JITx-Inc/open-components-database.git main --squash
+# git subtree pull -P ocdb git@github.com:JITx-Inc/open-components-database.git main --squash
 
 # old code from @philippe
-# cp -R ../../../open-components-database open-components-database
-# git add open-components-database
+# cp -R ../../../open-components-database ocdb
+# git add ocdb
 # git commit -m "Public release of JITX $TAG_NAME"
 # git branch --force main HEAD
 # git push --force
